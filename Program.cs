aa4383240a57a0adf85c3ac209ce7e8de6b63e27
Mahesaa <48425445+Mahesaa@users.noreply.github.com>
2019-03-28 20:05:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            mahasiswa Saya = new mahasiswa();
+            mahasiswa Anda = new mahasiswa();
+
+            Saya.Nim = "52465";
+            Saya.Nama = "Mahesa";
+            Saya.IPK = 1.86;
+
+            Anda.Nim = "15263";
+            Anda.Nama = "Af";
+            Anda.IPK = 2919;
+
+            Saya.Reg();
+            Saya.isikrs();
+
+            Anda.Reg();
+            Anda.isikrs();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan
+{
+    class mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string IPK { get; set; }
+
+        public void Reg()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("Nim: {0}", Nim);
+            Console.WriteLine("IPK: {0}", IPK);
+            Console.WriteLine("\nTelah melakukan Reg: {0}");
+        }
+
+        public void isikrs()
+        {
+            Console.WriteLine("{0}Sedang Melakukan proses KRS ", Nama);
+        }
+
+    }
+}
